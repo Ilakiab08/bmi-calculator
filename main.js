097566d05408d688abc98cb4a8f5119a68c5f9bc
Ilakiab08 <142892200+Ilakiab08@users.noreply.github.com>
2023-10-26 09:05:59 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+function calculateBmi(){
+
+    const weight = document.getElementById("weight").value;
+    const height = document.getElementById("height").value;
+    let result = document.querySelector("#result");
+    if (isNaN(weight) || isNaN(height) || weight <= 0|| height <= 50) {
+        result.textContent = "Please enter valid weight and height.";
+        
+} else{
+    /* const heightInMeters = height / 100;
+        const bmi = weight / (heightInMeters * heightInMeters); */
+        let bmi = (weight / ((height * height) / 10000)).toFixed(2);
+        if (bmi < 18.6) 
+        result =`Under Weight : ${bmi}`;
+
+    else if (bmi >= 18.6 && bmi < 24.9) 
+      result=  `Normal : ${bmi}`;
+
+    else result=
+        `Over Weight : ${bmi}`;
+return result;
+
+                }
+                   
+            }                  
